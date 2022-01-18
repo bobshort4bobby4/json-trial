a5fc01170091e24a66b2cf8d44dacac2d95ba01c
bobshort4bobby4 <treborken@gmail.com>
2022-01-18 17:43:16 +0000

    add creds.json

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import json
+
+var datafile = "5line-horsepro.json"
+
+horsedata = []
+
+def loaddata(datafile):
+    """
+    A function to load a json file into a variable
+    """
+    print("Please hold your horses!!.....loading data can take 20 seconds thanks")
+    for line in open(datafile,"r"):
+        #line = line.replace('None', "null")
+        horsedata.append(json.loads(line))
+
+loaddata(datafile)
+print(horsedata[0])
